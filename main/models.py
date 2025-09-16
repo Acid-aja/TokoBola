0a854d06f59fc9a8e0aa64f630dc0d13daea297a
Acid-aja <izzdn19@gmail.com>
2025-09-16 22:55:00 +0700

    progres tugas 2

--- main/models.py
+++ main/models.py
@@ -7,6 +7,12 @@ class Product(models.Model):
     thumbnail = models.URLField()
     category = models.CharField(max_length=50)
     is_featured = models.BooleanField(default=False)
+    views = models.PositiveIntegerField(default=0)
+
+    def increment_views(self):
+        """Tambah 1 ke jumlah views produk"""
+        self.views += 1
+        self.save(update_fields=["views"])
 
     def __str__(self):
         return self.name
