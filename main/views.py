8aca3291e06af8bb60dbf07f1bfee876fc966024
Acid-aja <izzdn19@gmail.com>
2025-09-17 08:07:18 +0700

    fix json by id dan xml by id

--- main/views.py
+++ main/views.py
@@ -48,7 +48,7 @@ def show_json(request):
 
 def show_xml_by_id(request, product_id):
    try:
-        product_list = Product.objects.all()
+        product_list = Product.objects.get(pk=product_id)
         xml_data = serializers.serialize("xml", product_list)
         return HttpResponse(xml_data, content_type="application/xml")
    except Product.DoesNotExist:
@@ -56,7 +56,7 @@ def show_xml_by_id(request, product_id):
    
 def show_json_by_id(request, product_id):
    try:
-        product_list = Product.objects.all()
+        product_list = Product.objects.get(pk=product_id)
         json_data = serializers.serialize("json", product_list)
         return HttpResponse(json_data, content_type="application/json")
    except Product.DoesNotExist:
